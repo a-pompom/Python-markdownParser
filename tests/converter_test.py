107068a026786d134aabcdaa41cf58e236437b4c
a-pompom <43694794+a-pompom@users.noreply.github.com>
2021-09-19 14:37:58 +0900

    パース結果の要素の構造を変えたことに伴い、テストコードを修正
    構造が変わったことにより、扱う型も変更されたので、入出力部分を調整した

--- tests/converter_test.py
+++ tests/converter_test.py
@@ -1,5 +1,7 @@
 from app.converter import Converter
-from app.markdown_parser import Block, ParseResult
+from app.markdown_parser import ParseResult
+from app.element.style import Plain
+from app.element.block import PlainBlock
 
 from tests.util import equal_for_parse_result
 
@@ -9,9 +11,9 @@ class TestConverter:
     def test_convert_plain(self):
         # GIVEN
         sut = Converter()
-        expected = ParseResult([Block('', ['HelloWorld'])])
+        expected = ParseResult([PlainBlock(Plain(), 'HelloWorld')])
 
         # WHEN
-        actual = sut.convert(ParseResult([Block('', ['HelloWorld'])]))
+        actual = sut.convert(ParseResult([PlainBlock(Plain(), 'HelloWorld')]))
         # THEN
         assert equal_for_parse_result(actual, expected)
--- tests/html_builder_test.py
+++ tests/html_builder_test.py
@@ -1,5 +1,9 @@
-from app.html_builder import HtmlBuilder
-from app.markdown_parser import ParseResult, Inline, Block
+import pytest
+
+from app.html_builder import HtmlBuilder, HeadingBuilder
+from app.markdown_parser import ParseResult
+from app.element.style import Plain, Heading
+from app.element.block import PlainBlock, HeadingBlock
 
 
 class TestHtmlBuilder:
@@ -9,10 +13,26 @@ class TestHtmlBuilder:
 
         # GIVEN
         expected = 'Hello World'
-        input = ParseResult([Block('', ['Hello World'])])
+        html_input = ParseResult([PlainBlock(Plain(), 'Hello World')])
         sut = HtmlBuilder()
 
         # WHEN
-        actual = sut.build(input)
+        actual = sut.build(html_input)
         # THEN
         assert actual == expected
+
+    class TestHeading:
+
+        @pytest.mark.parametrize(('block', 'expected'), [
+            (HeadingBlock(Heading(size=1), ['this is 1st heading']), '<h1>this is 1st heading</h1>'),
+            (HeadingBlock(Heading(size=4), ['this is 4th heading']), '<h4>this is 4th heading</h4>'),
+
+        ], ids=['1st heading', '4th heading'])
+        def test_build(self, block: HeadingBlock, expected: str):
+
+            # GIVEN
+            sut = HeadingBuilder()
+            # WHEN
+            actual = sut.build(block)
+            # THEN
+            assert actual == expected
