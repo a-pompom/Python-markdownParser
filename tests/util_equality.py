4e0190cd1743998c62bc4fbc0086a6eaeaffe0d3
a-pompom <43694794+a-pompom@users.noreply.github.com>
2021-10-09 16:22:45 +0900

    テストユーティリティとして、Block/Inline要素のファクトリを作成
    テストコードでは、Block/Inline要素をインスタンス化するとき、記述が冗長になっていた
    これは、インスタンスがパーサ内部でのみ扱うことを想定しており、テストコードに対応していないことによる
    かといってテスト用の処理を本筋の実装へ組み込むべきではないので、
    テストコードをよりシンプルにするためのユーティリティとして、ファクトリ関数をつくった

--- tests/util_equality.py
+++ tests/util_equality.py
@@ -17,6 +17,9 @@ def assert_that_text_file_content_is_same(expected_path: str, actual_path: str):
             assert actual.read() == expected.read()
 
 
+# パース結果の等価判定
+# 判定処理自体の妥当性は、各テストケースにより担保される
+# これは、結局判定処理のテストコードを書いても、評価するためのオブジェクトを生成する作業が個別のテストケースと変わらないためである
 def equal_for_inline(former: Inline, latter: Inline) -> bool:
     """
     Inline要素が等しいか検証
---
+++ tests/util_factory.py
@@ -0,0 +1,35 @@
+from app.element.block import Block, PlainBlock, HeadingBlock, Children
+from app.element.inline import Inline, PlainInline, LinkInline
+from app.element.style import Plain, Heading, Link
+
+
+def create_block(block_type: str, children: Children, **kwargs) -> Block:
+    """
+    パラメータをもとにBlock要素を生成
+
+    :param block_type: Blockの種類
+    :param children: 子Inline要素
+    :param kwargs: ヘッダの大きさなど、Block要素の付加属性
+    :return: 生成されたBlock要素
+    """
+
+    if block_type == 'heading':
+        return HeadingBlock(Heading(size=kwargs['size']), children)
+
+    return PlainBlock(Plain(), children)
+
+
+def create_inline(inline_type: str, text: str, **kwargs) -> Inline:
+    """
+    パラメータをもとにInline要素を生成
+
+    :param inline_type: Inlineの種類
+    :param text: 格納文字列
+    :param kwargs: リンクの参照など、Inline要素の付加属性
+    :return: 生成されたInline要素
+    """
+
+    if inline_type == 'link':
+        return LinkInline(Link(href=kwargs['href']), text)
+
+    return PlainInline(Plain(), text)
