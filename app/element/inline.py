10e76411774cf8589d65c8f8b32bbafa901526a4
a-pompom <43694794+a-pompom@users.noreply.github.com>
2021-11-07 17:47:42 +0900

    InlineParserのテストコードを__repr__()へ置き換え
    よりシンプルにテストデータを生成し、期待結果を比較するため、各Inline要素のクラスへ__repr__()を定義
    こうすることで、テストデータの期待結果は__repr__()で評価された文字列となり、
    検証処理も、評価された文字列に対する単純な等価比較で完結する

--- app/element/inline.py
+++ app/element/inline.py
@@ -14,6 +14,9 @@ class PlainInline(Inline):
     """ どの記法にも属さないInline要素 """
     style: Plain
 
+    def __repr__(self):
+        return f'Plain: text={self.text}'
+
 
 @dataclasses.dataclass
 class LinkInline(Inline):
@@ -29,7 +32,13 @@ class CodeInline(Inline):
     """ codeタグと対応するコード要素を保持 """
     style: Code
 
+    def __repr__(self):
+        return f'Code: text={self.text}'
+
 
 class ImageInline(Inline):
     """ imgタグと対応する画像要素を保持 """
     style: Image
+
+    def __repr__(self):
+        return f'Image: src={self.style.src}, alt={self.style.alt}'
--- tests/markdown/inline_parser_test.py
+++ tests/markdown/inline_parser_test.py
@@ -1,9 +1,7 @@
 import pytest
 
 from app.markdown.inline_parser import InlineParser, LinkParser, CodeParser, ImageParser
-from app.element.inline import Inline, CodeInline, ImageInline
-from tests.util_equality import equal_for_inline_parse_result, equal_for_inline
-from tests.util_factory import create_inline
+from app.element.inline import Inline
 
 
 def assert_for_inline_parse_result(actual: tuple[str, Inline, str], expected: tuple[str, str, str]):
@@ -30,19 +28,19 @@ class TestInlineParser:
     @pytest.mark.parametrize(('text', 'expected'), [
         (
                 'plain text',
-                [create_inline('', 'plain text')]
+                ['Plain: text=plain text']
         ),
         (
                 'this is [google link](https://www.google.com/)',
-                [create_inline('', 'this is '), create_inline('link', 'google link', href='https://www.google.com/')]
+                ['Plain: text=this is ', 'Link: text= google link, href=https://www.google.com/']
         )
     ], ids=['plain', 'link'])
-    def test_parse(self, text: str, expected: list[Inline]):
+    def test_parse(self, text: str, expected: list[str]):
         sut = InlineParser()
         actual = sut.parse(text)
 
-        for former, latter in zip(actual, expected):
-            assert equal_for_inline(former, latter)
+        for actual_inline, expected_text in zip(actual, expected):
+            repr(actual_inline) == expected_text
 
 
 class TestLink:
@@ -100,10 +98,13 @@ class TestCode:
     """ ``で表現されるコード要素を検証 """
 
     # 記法が対象か
-    @pytest.mark.parametrize(('text', 'expected'), [
-        ('Pythonのコメントは`#`で表現されます。', True),
-        ('私はPythonが好きです', False),
-    ], ids=['code', 'not code'])
+    @pytest.mark.parametrize(
+        ('text', 'expected'),
+        [
+            ('Pythonのコメントは`#`で表現されます。', True),
+            ('私はPythonが好きです', False),
+        ],
+        ids=['code', 'not code'])
     def test_target(self, text: str, expected: bool):
         # GIVEN
         sut = CodeParser()
@@ -113,28 +114,43 @@ class TestCode:
         assert actual == expected
 
     # 記法を解釈
-    @pytest.mark.parametrize(('text', 'expected'), [
-        ('`//`でコメントを表現します。', ['', create_inline('code', '//'), 'でコメントを表現します。']),
-        ('JavaScriptの変数は`const`で宣言します。', ['JavaScriptの変数は', create_inline('code', 'const'), 'で宣言します。']),
-        ('codeで終わります`。`', ['codeで終わります', create_inline('code', '。'), '']),
-    ], ids=['no head', 'both', 'no tail'])
-    def test_parse(self, text: str, expected: tuple[str, CodeInline, str]):
+    @pytest.mark.parametrize(
+        ('text', 'expected'),
+        [
+            (
+                    '`//`でコメントを表現します。',
+                    ('', 'Code: text=//', 'でコメントを表現します。')
+            ),
+            (
+                    'JavaScriptの変数は`const`で宣言します。',
+                    ('JavaScriptの変数は', 'Code: text=const', 'で宣言します。')
+            ),
+            (
+                    'codeで終わります`。`',
+                    ('codeで終わります', 'Code: text=。', '')
+            ),
+        ],
+        ids=['no head', 'both', 'no tail'])
+    def test_parse(self, text: str, expected: tuple[str, str, str]):
         # GIVEN
         sut = CodeParser()
         # WHEN
         actual = sut.parse(text)
         # THEN
-        assert equal_for_inline_parse_result(actual, expected)
+        assert_for_inline_parse_result(actual, expected)
 
 
 class TestImage:
     """ ![]()で表現される画像要素を検証 """
 
     # 対象判定
-    @pytest.mark.parametrize(('text', 'expected'), [
-        ('![画像](img.png)', True),
-        ('[これはリンクです](https://www.google.com', False)
-    ], ids=['target', 'not target'])
+    @pytest.mark.parametrize(
+        ('text', 'expected'),
+        [
+            ('![画像](img.png)', True),
+            ('[これはリンクです](https://www.google.com', False)
+        ],
+        ids=['target', 'not target'])
     def test_target(self, text: str, expected: bool):
         # GIVEN
         sut = ImageParser()
@@ -144,36 +160,27 @@ class TestImage:
         assert actual == expected
 
     # 記法を解釈
-    @pytest.mark.parametrize(('text', 'expected'), [
-        (
-                '![awesome image](/image.png) is here.',
-                (
-                        '',
-                        create_inline('image', '', src='/image.png', alt='awesome image'),
-                        ' is here.',
-                )
-        ),
-        (
-                'このアイコン![アイコン](/image/icon.png)は良いですね。',
-                (
-                        'このアイコン',
-                        create_inline('image', '', src='/image/icon.png', alt='アイコン'),
-                        'は良いですね。'
-                )
-        ),
-        (
-                '最新の画像![画像](/画像.png)',
-                (
-                        '最新の画像',
-                        create_inline('image', '', src='/画像.png', alt='画像'),
-                        '',
-                )
-        ),
-    ], ids=['head', 'both', 'tail'])
-    def test_parse(self, text: str, expected: tuple[str, ImageInline, str]):
+    @pytest.mark.parametrize(
+        ('text', 'expected'),
+        [
+            (
+                    '![awesome image](/image.png) is here.',
+                    ('', 'Image: src=/image.png, alt=awesome image', ' is here.')
+            ),
+            (
+                    'このアイコン![アイコン](/image/icon.png)は良いですね。',
+                    ('このアイコン', 'Image: src=/image/icon.png, alt=アイコン', 'は良いですね。')
+            ),
+            (
+                    '最新の画像![画像](/画像.png)',
+                    ('最新の画像', 'Image: src=/画像.png, alt=画像', '')
+            ),
+        ],
+        ids=['head', 'both', 'tail'])
+    def test_parse(self, text: str, expected: tuple[str, str, str]):
         # GIVEN
         sut = ImageParser()
         # WHEN
         actual = sut.parse(text)
         # THEN
-        assert equal_for_inline_parse_result(actual, expected)
+        assert_for_inline_parse_result(actual, expected)
