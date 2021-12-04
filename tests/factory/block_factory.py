01c29bf8973db131cdfdfb1391a954d994b9b133
a-pompom <43694794+a-pompom@users.noreply.github.com>
2021-12-04 21:00:06 +0900

    [#18] ListItemのBlock要素を生成するテストファクトリを作成
    ListItemのBlock要素は、コンバータにより、ListのBlock要素の子として生成される
    これをテスト済みのモジュールからつくり、単体で取り出そうとすると、無理やりな処理を書かなければならない
    それならばテストデータのファクトリを作成し、テストコードも書いたものを利用した方が
    全体的にシンプルに収まると考えた
    よって、新たにテストファクトリを導入した

---
+++ tests/factory/block_factory.py
@@ -0,0 +1,30 @@
+from app.element.style import ListItem
+from app.element.block import ListItemBlock
+from app.markdown.inline_parser import InlineParser
+
+
+# Block要素のファクトリクラスは、コンバータでしか生成できないなど、インスタンスを
+# 生成するのが困難なものに限定
+# テスト済みのモジュールを介してテストデータを生成するよりもコストが掛かると判断した場合のみ、ファクトリクラスを定義する
+
+class ListItemFactory:
+    """ リストの子を表現するBlock要素を生成 """
+
+    def create_single_list_item(self, text: str) -> ListItemBlock:
+        """
+        単一のリスト子要素を生成
+
+        :param text: リスト子要素の文字列
+        :return: ListItemBlock要素
+        """
+        return ListItemBlock(ListItem(), InlineParser().parse(text))
+
+    def create_multiple_list_item(self, lines: list[str]) -> list[ListItemBlock]:
+        """
+        複数のリスト子要素を生成
+
+        :param lines: 各リスト子要素の文字列を格納
+        :return: ListItemBlock要素のリスト
+        """
+        return [ListItemBlock(ListItem(), InlineParser().parse(line)) for line in lines]
+
