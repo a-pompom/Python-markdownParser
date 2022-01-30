de7ad3fcc5bf9f99f512713dc28aa7ed471bda11
a-pompom <43694794+a-pompom@users.noreply.github.com>
2022-01-30 11:04:12 +0900

    [#41] パッケージ化に伴い、setup.pyファイルを作成

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="a_pompom_markdown_parser",
+    version="1.0.0",
+    author="a-pompom",
+    author_email="",
+    description="",
+    url="https://github.com/a-pompom/Python-markdownParser",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': ['a_pompom_markdown_parse = a_pompom_markdown_parser.main:main']
+    },
+    python_requires='>=3.10',
+)
