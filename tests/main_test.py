c5f8f5891eb68a8f0ddafe54d967198840fe37f8
a-pompom <43694794+a-pompom@users.noreply.github.com>
2022-05-29 15:56:48 +0900

    [#54] 文字列を入出力にマークダウンをHTMLへ変換する処理のテストコードを作成

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,7 +1,11 @@
-from a_pompom_markdown_parser.main import parse_md_to_html
+import pytest
+from a_pompom_markdown_parser.main import parse_md_to_html, parse_md_to_html_by_string
+from a_pompom_markdown_parser.settings import setting
 
 from tests.util_equality import assert_that_text_file_content_is_same
 
+
+LINE_BREAK = setting['newline_code']
 
 class TestMain:
 
@@ -55,3 +59,28 @@ class TestMain:
         parse_md_to_html(text_path['input'], text_path['output'])
         # THEN
         assert_that_text_file_content_is_same(text_path['expected'], text_path['output'])
+
+
+class TestParseByString:
+    """ 入出力を文字列とし、マークダウン要素をHTML要素へパースできるか検証 """
+
+    # HTML文字列を組み立てられるか
+    @pytest.mark.parametrize(
+        ('markdown_content_path', 'expected_path'),
+        [
+            (
+                './template/markdown/sample_article.md',
+                './template/html/sample_article.html'
+            )
+        ]
+    )
+    def test_parse_by_string(self, markdown_content_path: str, expected_path: str):
+        # GIVEN
+        sut = parse_md_to_html_by_string
+        # WHEN
+        with open(markdown_content_path, 'r') as f:
+            markdown_content = f.read()
+            actual = sut(markdown_content)
+            with open(expected_path, 'r') as f_expected:
+                # THEN
+                assert actual == f_expected.read()
