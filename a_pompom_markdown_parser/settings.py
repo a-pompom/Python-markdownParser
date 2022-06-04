96b119da7b6095dc311026b0cd6a94c703f681a6
a-pompom <43694794+a-pompom@users.noreply.github.com>
2022-06-04 10:49:23 +0900

    [#56] h4・hrのスタイルを変更
    h4タグは文字が小さい分、本文と見分けがつきにくく、境界が曖昧になっていた
    よって、下線を追加することで境界を明確にした
    また、hrタグは余白が少なく、これもまた境界が曖昧になっていた
    あわせて余白を広げることで境目を見えやすくした

--- a_pompom_markdown_parser/settings.py
+++ a_pompom_markdown_parser/settings.py
@@ -8,7 +8,7 @@ setting = {
         'h1': 'mt-10 mb-10 text-4xl font-sans font-bold border-solid border-indigo-400 border-b-2 pb-1.5',
         'h2': 'pb-1 mt-8 mb-8 font-sans text-2xl font-semibold border-b-2 border-indigo-400 border-solid',
         'h3': 'mt-6 mb-6 font-sans text-xl font-semibold',
-        'h4': 'mt-4 mb-4 text-lg',
+        'h4': 'mt-4 mb-4 text-lg border-b border-indigo-400 border-solid pb-1',
         'p': 'mt-2 mb-2',
 
         'ul': 'mt-4 ml-8 list-disc',
@@ -16,7 +16,7 @@ setting = {
         # ネストしているときliへ適用したいスタイル
         'li_nested': 'mt-2 list-none',
         'blockquote': 'pl-4 border-l-2 border-gray-400 border-solid text-slate-400',
-        'hr': 'border-b-2 border-indigo-400',
+        'hr': 'border-b-2 border-indigo-400 mt-6 mb-6',
 
         'code': 'bg-slate-700',
         'a': 'text-sky-300',
